68aa744c8ecf15fa9dcc326b27e7573371702d50
Chiragsinh933 <chiragsinh.jadeja128207@marwadiuniversity.ac.in>
2026-07-30 22:17:59 +0530

    Time: 7 ms (95.37%), Space: 56.9 MB (94.22%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    // Merge two sorted linked lists
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (l1 != NULL && l2 != NULL) {
+
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        if (l1 != NULL)
+            tail->next = l1;
+        else
+            tail->next = l2;
+
+        return dummy.next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* mid = slow->next;
+        slow->next = NULL;
+
+        // Sort left and right halves
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+
+        // Merge them
+        return merge(left, right);
+    }
+};

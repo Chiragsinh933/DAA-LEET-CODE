dce75c11cd58a11052ae1ef6c3927de31065b801
Chiragsinh933 <chiragsinh.jadeja128207@marwadiuniversity.ac.in>
2026-08-25 21:39:05 +0530

    Time: 47 ms (88.42%), Space: 173.8 MB (97.72%) - LeetHub

---
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        if(k == 0) {
+            return n;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+
+        for(int i = 0; i < n - k; i++) {
+
+            if(nums[i] < nums[n - k]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};

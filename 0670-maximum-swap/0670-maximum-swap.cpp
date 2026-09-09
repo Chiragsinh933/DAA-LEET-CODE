585b1b75557af3927c79d669c191b3ddf672d068
Chiragsinh933 <chiragsinh.jadeja128207@marwadiuniversity.ac.in>
2026-09-09 09:24:56 +0530

    Time: 0 ms (100%), Space: 8 MB (63.42%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -8,14 +8,12 @@ public:
             
             int maxIndex = i;
 
-            // Find the largest digit after i
             for (int j = i + 1; j < s.length(); j++) {
                 if (s[j] >= s[maxIndex]) {
                     maxIndex = j;
                 }
             }
 
-            // If a bigger digit is found, swap
             if (s[maxIndex] > s[i]) {
                 swap(s[i], s[maxIndex]);
                 break;
